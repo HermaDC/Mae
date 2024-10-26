97383617b372be722a0044733adcf8adb1e67c2d
hermaDC <diazcabezas.m1@gmail.com>
2024-10-26 20:29:03 +0200

    Initial commit in branch main

---
+++ main.js
@@ -0,0 +1,32 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+let interval; // Variable para almacenar el intervalo de tiempo
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
+  });
+  updateDots(index);
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  showSlide(currentIndex);
+}
+
+function goToSlide(index) {
+  clearInterval(interval); // Detiene el intervalo cuando se pulsa un punto
+  currentIndex = index;
+  showSlide(currentIndex);
+}
+
+function updateDots(index) {
+  dots.forEach((dot, i) => {
+    dot.classList.toggle("active", i === index);
+  });
+}
+
+// Inicia el slider y cambia cada 3 segundos
+interval = setInterval(nextSlide, 3000);
+showSlide(currentIndex);
